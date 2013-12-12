d664da046f5807ebd26c38f24130c0531dec5dd4
Christopher Thielen <cthielen@gmail.com>
2013-12-12 11:38:55 -0800

    Added name field to checklists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131212183016) do
+ActiveRecord::Schema.define(version: 20131212193701) do
 
   create_table "checklist_entries", force: true do |t|
     t.integer  "checklist_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20131212183016) do
     t.datetime "finished"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "template_entries", force: true do |t|
