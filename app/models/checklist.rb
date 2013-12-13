45cfbfae8e570a1eded68a8e571fd79b2e3b75b0
Christopher Thielen <cthielen@gmail.com>
2013-12-12 18:38:28 -0800

    Add dependeny => destroy to appropriate models

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -3,7 +3,7 @@ class Checklist < ActiveRecord::Base
   
   belongs_to :template
   belongs_to :user
-  has_many :entries, :class_name => 'ChecklistEntry'
+  has_many :entries, :class_name => 'ChecklistEntry', :dependent => :destroy
   
   validates_presence_of :template, :user_id, :name
   validates :public, :inclusion => {:in => [true, false]}
--- app/models/template.rb
+++ app/models/template.rb
@@ -2,7 +2,7 @@ class Template < ActiveRecord::Base
   using_access_control
   
   belongs_to :owner, :class_name => 'User'
-  has_many :entries, :class_name => 'TemplateEntry'
+  has_many :entries, :class_name => 'TemplateEntry', :dependent => :destroy
   
   validates_presence_of :owner, :name
   accepts_nested_attributes_for :entries
