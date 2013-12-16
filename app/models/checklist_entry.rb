6879153a6efe46254a0cefd1b351eaca33c1dd07
Christopher Thielen <cthielen@gmail.com>
2013-12-16 10:24:26 -0800

    Clear user_id in ChecklistEntry when unchecking

--- app/models/checklist_entry.rb
+++ app/models/checklist_entry.rb
@@ -5,4 +5,11 @@ class ChecklistEntry < ActiveRecord::Base
   belongs_to :checklist
   
   validates_presence_of :checklist
+  
+  before_save :clear_user_id_if_unchecking
+  
+  private
+  def clear_user_id_if_unchecking
+    self.user_id = nil if self.checked == false
+  end
 end
