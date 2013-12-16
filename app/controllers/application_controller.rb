9b7b06486070d16c784f5e5e7cd92d57f06fe3c2
Christopher Thielen <cthielen@gmail.com>
2013-12-16 11:32:22 -0800

    Redirect to access_denied on delcarative_authorization error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def permission_denied
+    if session[:auth_via] == :cas
+      # Human-facing error
+      flash[:error] = "Sorry, you are not allowed to access that page."
+      redirect_to access_denied_path
+    else
+      # Machine-facing error
+      render :text => "Permission denied.", :status => 403
+    end
+  end
 end
