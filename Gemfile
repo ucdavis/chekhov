b69dbd08b4ff485c2ef9cd0de5704401d718868e
Christopher Thielen <cthielen@gmail.com>
2014-01-17 11:36:27 -0800

    Require SysAid gem and log into SysAid on application start

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
+# For SysAid support
+gem 'sysaid', '>= 0.2.3'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 
--- config/initializers/sysaid.rb
+++ config/initializers/sysaid.rb
@@ -2,6 +2,8 @@ SYSAID_SETTINGS_FILE = Rails.root.join( "config", "sysaid.yml")
 
 if File.file?(SYSAID_SETTINGS_FILE)
   SYSAID_SETTINGS = YAML.load_file(SYSAID_SETTINGS_FILE)
+  
+  SysAid::login SYSAID_SETTINGS["ACCOUNT"], SYSAID_SETTINGS["USER"], SYSAID_SETTINGS["PASSWORD"], SYSAID_SETTINGS["WSDL_URI"]
 else
   puts "You need to set up config/sysaid.yml before running this application."
   exit
