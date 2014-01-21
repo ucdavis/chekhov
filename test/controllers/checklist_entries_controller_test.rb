11cd61b1755d354ec2621696d19ca98baa2a7ba0
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-01-21 13:20:59 -0800

    Removing controller test scaffolding

--- test/controllers/checklist_entries_controller_test.rb
+++ test/controllers/checklist_entries_controller_test.rb
@@ -5,45 +5,6 @@ class ChecklistEntriesControllerTest < ActionController::TestCase
     @checklist_entry = checklist_entries(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:checklist_entries)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create checklist_entry" do
-    assert_difference('ChecklistEntry.count') do
-      post :create, checklist_entry: { checklist_id: @checklist_entry.checklist_id, template_entry_id: @checklist_entry.template_entry_id, user_id: @checklist_entry.user_id }
-    end
-
-    assert_redirected_to checklist_entry_path(assigns(:checklist_entry))
-  end
-
-  test "should show checklist_entry" do
-    get :show, id: @checklist_entry
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @checklist_entry
-    assert_response :success
-  end
-
-  test "should update checklist_entry" do
-    patch :update, id: @checklist_entry, checklist_entry: { checklist_id: @checklist_entry.checklist_id, template_entry_id: @checklist_entry.template_entry_id, user_id: @checklist_entry.user_id }
-    assert_redirected_to checklist_entry_path(assigns(:checklist_entry))
-  end
-
-  test "should destroy checklist_entry" do
-    assert_difference('ChecklistEntry.count', -1) do
-      delete :destroy, id: @checklist_entry
-    end
-
     assert_redirected_to checklist_entries_path
   end
 end
