bf441cfd00bdaba3d54f97ba2f6ccf56df214bf4
Obada Kadri <okadri@ucdavis.edu>
2014-01-21 19:02:44 -0800

    Handle error when saving to a non-existent ticket

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -63,7 +63,7 @@ class ChecklistsController < ApplicationController
       flash[:notice] = "Could not update checklist: #{e}."
       
       respond_to do |format|
-        format.json { render :json => {:errors => {:SysAid => ["Could not save comment with the SysAid server."]}}, status: 422 }
+        format.json { render :json => {:errors => {:SysAid => ["#{e}"]}}, status: 422 }
       end
     end
   end
@@ -104,11 +104,16 @@ class ChecklistsController < ApplicationController
           
           unless params[:checklist][:ticket_number].blank?
             ticket = SysAid::Ticket.find_by_id params[:checklist][:ticket_number]
-            ticket.add_note c[:author], c[:content]
 
-            unless ticket.save
-              raise SysAidError, "failed to save new comment to SysAid"
+            if ticket
+              ticket.add_note c[:author], c[:content]
+              unless ticket.save
+                raise SysAidError, "Failed to save new comment to SysAid"
+              end
+            else
+              raise SysAidError, "No ticket matches the provided ticket number"
             end
+
           end
         end
       end if params[:checklist][:comments_attributes]
