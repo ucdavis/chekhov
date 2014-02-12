b9357da79d8ef78b43b83ac12bc0ec986931c0aa
Christopher Thielen <cthielen@gmail.com>
2014-02-11 17:19:14 -0800

    Updated to SysAid 0.3.2 gem and modified SysAid initializer to avoid logging in on initializer. This avoids talking to the SysAid server just to do simple tasks like running a db:migrate.

--- config/initializers/sysaid.rb
+++ config/initializers/sysaid.rb
@@ -6,8 +6,7 @@ SYSAID_SETTINGS_FILE = Rails.root.join( "config", "sysaid.yml")
 
 if File.file?(SYSAID_SETTINGS_FILE)
   SYSAID_SETTINGS = YAML.load_file(SYSAID_SETTINGS_FILE)
-  
-  SysAid::login SYSAID_SETTINGS["ACCOUNT"], SYSAID_SETTINGS["USER"], SYSAID_SETTINGS["PASSWORD"], SYSAID_SETTINGS["WSDL_URI"]
+  SysAid::server_settings = { account: SYSAID_SETTINGS["ACCOUNT"], username: SYSAID_SETTINGS["USER"], password: SYSAID_SETTINGS["PASSWORD"], wsdl_uri: SYSAID_SETTINGS["WSDL_URI"] }
 else
   puts "You need to set up config/sysaid.yml before running this application."
   exit
