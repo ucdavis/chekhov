5145c3a3ef09ec2d16a4733c6b1cdc9dd5a8e317
Obada Kadri <okadri@ucdavis.edu>
2014-03-10 17:00:01 -0700

    Created /auth route as a redirect for all login requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include Authentication
+  before_filter :require_login
   before_action :authenticate
   
   # Prevent CSRF attacks by raising an exception.
@@ -18,5 +19,23 @@ class ApplicationController < ActionController::Base
       # Non-JSON, machine-facing error
       render :text => "Permission denied.", :status => 403
     end
+  end
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to auth_url({req: request.original_url}) # halts request cycle
+    end
+  end
+
+  # The logged_in? method simply returns true if the user is logged
+  # in and false otherwise. It does this by "booleanizing" the
+  # current_user method we created previously using a double ! operator.
+  # Note that this is not common in Ruby and is discouraged unless you
+  # really mean to convert something into true or false.
+  def logged_in?
+    !!current_user
   end
 end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,6 @@
 class SiteController < ApplicationController
   skip_before_filter :authenticate, :only => [:welcome, :access_denied]
+  skip_before_filter :require_login, :only => [:welcome, :access_denied, :auth]
   
   def welcome
     respond_to do |format|
@@ -8,5 +9,9 @@ class SiteController < ApplicationController
   end
 
   def access_denied
+  end
+
+  def auth
+    redirect_to params['req']
   end
 end
