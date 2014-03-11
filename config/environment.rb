d1e09c053a3bf1e68d63317189303bc66395703d
Obada Kadri <okadri@ucdavis.edu>
2014-03-10 19:17:23 -0700

    Enabled CAS logger

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,12 @@ require "casclient/frameworks/rails/filter"
 # Initialize the Rails application.
 Chekhov::Application.initialize!
 
+# enable detailed CAS logging
+cas_logger = CASClient::Logger.new(::Rails.root+'log/cas.log')
+cas_logger.level = Logger::DEBUG
+
 CASClient::Frameworks::Rails::Filter.configure(
   :cas_base_url => "https://cas.ucdavis.edu/cas/",
+  :logger => cas_logger,
   :enable_single_sign_out => true
 )
