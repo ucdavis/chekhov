aceaacbe52e711a1576e36120cdd70b631a2c9df
Iris Chan <iwichan@ucdavis.edu>
2014-10-22 15:21:42 -0700

    #48, created basic status page and added routes/setup

---
+++ app/views/site/status.json.jbuilder
@@ -0,0 +1,2 @@
+json.checklists @number_of_checklists
+json.status, format_content("ok")
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Chekhov::Application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   get '/welcome' => 'site#welcome'
   get '/access_denied' => 'site#access_denied'
+  get '/status' => 'site#status', defaults: { format: 'json'}
 
   match '/auth' => 'site#auth', via: [:get, :post]
   
