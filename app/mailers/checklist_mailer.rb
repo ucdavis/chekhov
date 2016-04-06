9670ca9b93a2c3d7eea65f91a8b567bae02ff682
Mark Diez <markediez@gmail.com>
2016-04-06 16:39:44 -0700

    add method to send abandoned

--- app/mailers/checklist_mailer.rb
+++ app/mailers/checklist_mailer.rb
@@ -1,8 +1,13 @@
 class ChecklistMailer < ActionMailer::Base
   default from: "no-reply@dss.ucdavis.edu"
-  
+
   def send_email(message, recipient, subject)
     @message = message
     mail(:subject => subject, :to => recipient)
+  end
+
+  def send_abandoned(checklist, recipient)
+    @checklist = checklist
+    mail(:subject => "Abandoned Checklist", :to => recipient)
   end
 end
