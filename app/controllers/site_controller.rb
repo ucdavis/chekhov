174dac4a66438ee1ccc5ef67714eed1f0076777f
Mark Diez <markediez@gmail.com>
2016-04-07 16:02:55 -0700

    Define action to redirect to checklist

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,12 @@ class SiteController < ApplicationController
   skip_before_filter :authenticate, :only => [:welcome, :access_denied, :status]
   skip_before_filter :require_login, :only => [:welcome, :access_denied, :auth, :status]
 
+  def show_checklist
+    checklist_num = params[:id]
+    url = "/#/checklists/" + checklist_num
+    redirect_to url
+  end
+
   def access_denied
   end
 
@@ -9,10 +15,10 @@ class SiteController < ApplicationController
     req = params['req'].blank? ? templates_url : params['req']
     redirect_to req
   end
-  
+
   def status
       @number_of_checklists = Checklist.all.count
-      
+
       respond_to do |format|
           format.json { render :json => {NumberOfChecklists: @number_of_checklists, Status: "OK"}}
       end
